085509e584fb12b7ac2b0c8d70f5c687e9f0ddeb
KalutaraDon <zuhry.fayesz@gmail.com>
2024-08-14 00:24:40 +0530

    changes

---
+++ src/components/InfiniteScrollGrid.tsx
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+
+const InfiniteScrollGrid: React.FC = () => {
+
+  return (
+    <>
+      <div>hi</div>
+    </>
+  );
+};
+
+export default InfiniteScrollGrid;
